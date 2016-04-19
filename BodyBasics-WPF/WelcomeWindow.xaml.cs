e2cc2047298467a8a2996dcf423ff3e40d66a8b7
Jocsan Macias <macias.jocsan@hotmail.com>
2016-04-19 18:11:20 -0400

    login logic

--- BodyBasics-WPF/WelcomeWindow.xaml.cs
+++ BodyBasics-WPF/WelcomeWindow.xaml.cs
@@ -11,24 +11,100 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Configuration;
 
 namespace Microsoft.Samples.Kinect.BodyBasics
 {
+
+	
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
     public partial class Window1 : Window
     {
+        private bool login = false;
+        private string connectionString = "";
+
         public Window1()
         {
             InitializeComponent();
-          
+            string returnValue = null;
+
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["md_connection"];
+
+
+            if (settings != null)
+                returnValue = settings.ConnectionString;
+
+            connectionString = returnValue;
         }
 
+        
+
+		public void DoLogin() {
+			string username = "";
+			string password = ""; //add input logic here. Messagebox should work
+
+
+			
+			// Schema as follows. Make it in your DB:
+			//		User
+			//			- id
+			//			- name
+			//			- password
+			//			- hint
+			using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "SELECT * FROM user where password=@pass AND name=@user";
+                cmd.Prepare();
+                cmd.Parameters.Add("@pass", MySqlDbType.Text).Value = password;
+				cmd.Parameters.Add("@user", MySqlDbType.Text).Value = username;
+
+                using (MySqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+						login = true; // will be true if theres anything to read
+                    }
+                }
+            }
+		}
+		
+		public void RegisterUser() {
+			// Add input data here
+			string id, name, pass, hint;
+			
+			using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "INSERT INTO user (id,name,password,hint) VALUES (@id,@name,@pass,@hint,)";
+                cmd.Parameters.AddWithValue("@pid", id);
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@pass", pass);
+                cmd.Parameters.AddWithValue("@hint", hint);			
+                cmd.ExecuteNonQuery();
+
+            }
+		}
+		
         public void GoToExercises(object sender, RoutedEventArgs e)
         {
-            ExerciseChoice ec = new ExerciseChoice();
-            ec.Show();
+			DoLogin();
+			if(login) {
+				ExerciseChoice ec = new ExerciseChoice();
+				ec.Show();
+			} else {
+				MessageBox.Show("Username or Password is incorrect");
+				// add hint here if we want to?
+			}
         }
 
         public void ToDoAlert(object sender, RoutedEventArgs e)
@@ -43,3 +119,4 @@ namespace Microsoft.Samples.Kinect.BodyBasics
         }
     }
 }
+
